e3326a32dc62ec3ed3775acd9d7e61673cb61317
hulefei <hulefei29@gmail.com>
2023-10-27 15:00:09 +0800

    [add]天 entrypoint.sh

--- download/download.sh
+++ download/download.sh
@@ -1,23 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -Eeuo pipefail
-
-# TODO: maybe just use the .gitignore file to create all of these
-# mkdir -vp /data/.cache \
-#   /data/embeddings \
-#   /data/config/ \
-#   /data/models/ \
-#   /data/models/Stable-diffusion \
-#   /data/models/GFPGAN \
-#   /data/models/RealESRGAN \
-#   /data/models/LDSR \
-#   /data/models/VAE
-
-echo "Downloading, this might take a while..."
-
-aria2c -x 10 --disable-ipv6 --input-file /download/links.txt --dir /models --continue
-
-echo "Checking SHAs..."
-
-# parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
-
+# This is a comment
+echo "Hello, World! bash"
